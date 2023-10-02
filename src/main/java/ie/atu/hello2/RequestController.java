8c9dab1defdefec6b1c8a0733c8c4ca897b2aaab
PATRICK HESSION <G00394619@gmit.ie>
2023-10-02 17:52:17 +0100

    Initial commit

---
+++ src/main/java/ie/atu/hello2/RequestController.java
@@ -0,0 +1,11 @@
+package ie.atu.hello2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RequestController {
+
+  
+}
+
