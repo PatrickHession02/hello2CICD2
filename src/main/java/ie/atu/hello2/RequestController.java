8d2f8c37a073f5afa540fbaa9b82f33d3c274685
PATRICK HESSION <G00394619@gmit.ie>
2023-10-02 19:30:55 +0100

    Added Hello And Greeting controllelrs

--- src/main/java/ie/atu/hello2/RequestController.java
+++ src/main/java/ie/atu/hello2/RequestController.java
@@ -4,8 +4,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class RequestController {
 
-  
+
+
+public class RequestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+    @GetMapping("/greet/{name}")
+    public String greetByName(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
+
+
 }
 
