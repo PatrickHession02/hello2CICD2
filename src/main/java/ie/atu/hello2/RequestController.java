6f4ac061f8b853f53cef7c2a2a5bad67a7858307
PATRICK HESSION <G00394619@gmit.ie>
2023-10-02 21:32:23 +0100

    Added details

--- src/main/java/ie/atu/hello2/RequestController.java
+++ src/main/java/ie/atu/hello2/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.hello2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 
@@ -17,6 +18,12 @@ public class RequestController {
         return "Hello, " + name + "!";
     }
 
-
+    @GetMapping("/details")
+    public String details(
+            @RequestParam String name,
+            @RequestParam int age) {
+        String message = "Name: " + name + ", Age: " + age;
+        return message;
+    }
 }
 
