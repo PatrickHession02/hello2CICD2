f870d99a797ff8280f9435b6bfa5e6cf83532301
PATRICK HESSION <G00394619@gmit.ie>
2023-10-03 08:52:13 +0100

    Added different cases and comments

--- src/main/java/ie/atu/hello2/CalculatorController.java
+++ src/main/java/ie/atu/hello2/CalculatorController.java
@@ -1,8 +1,8 @@
 package ie.atu.hello2;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class CalculatorController {
@@ -14,4 +14,33 @@ public class CalculatorController {
             @RequestParam("operation") String operation
     )
     {
-       
+        double Result = 0;
+
+        switch (operation) {
+            case "Add":   //Adds received numbers
+
+                Result = num1 + num2;
+                break;
+
+            case "Subtract": //Subtracts received Number
+                Result = num1 - num2;
+                break;
+
+            case "Multiply":
+                Result = num1 * num2; //Multiplies received Number
+                break;
+
+            case "Divide":   //Divides received Number
+                if (num2 != 0) {
+                    Result = num1 / num2;
+                } else {
+                    throw new IllegalArgumentException("Cannot Divide By Zero.");  //Prevents user from dividing by 0
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid Action: " + operation); //default error
+        }
+
+        return new calculatorResult(operation, Result); //returns JSON
+    }
+}
